617df905829ce7e8593463a7206c35e45e846dbf
unknown <suchethgr@gmail.com>
2020-06-25 13:02:17 +0530

    added some more functionalities

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 TextView timerTextView;
 Button startButton;
@@ -17,6 +19,7 @@ Button resetButton;
 Button stopButton;
 private int seconds = 0;
 private boolean running = false;
+private boolean wasRunning = false;
 public void startButton(View view){
     startButton.setVisibility(View.INVISIBLE);
     Log.i("Startbutton clicked","pressed start");
@@ -25,9 +28,7 @@ public void startButton(View view){
     running = true;
 }
 public void stopButton(View view){
-    startButton.setVisibility(View.VISIBLE);
-    resetButton.setVisibility(View.INVISIBLE);
-    stopButton.setVisibility(View.INVISIBLE);
+
     running = false;
 }
 public void resetButton(View view){
@@ -37,22 +38,44 @@ public void resetButton(View view){
     running  =false;
     seconds = 0;
 }
+
+
 public void runTimer(){
-if(running)
-{
-    Handler handler = new Handler();
-    Runnable run = new Runnable() {
+    final Handler handler = new Handler();
+    handler.post(new Runnable() {
         @Override
         public void run() {
+       int hrs = seconds/3600;
+       int min = (seconds%3600)/60;
+       int sec  = seconds%60;
 
+       String setTime = String.format(Locale.getDefault(),"%d:%02d:%02d", hrs,min, sec);
+       timerTextView.setText(setTime);
+       if (running){
+           seconds++;
+       }
+       handler.postDelayed(this,1000);
         }
-    };
+
+    });
+
 
 }
-else {
+public void onPause() {
+    super.onPause();
+    wasRunning = running;
+    running = false;
+}
+public void onResume() {
+    super.onResume();
+    if (wasRunning) {
+        running = true;
+    }
+}
 
-}
-}
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,5 +88,26 @@ else {
         resetButton.setVisibility(View.INVISIBLE);
         stopButton.setVisibility(View.INVISIBLE);
 
+        if(savedInstanceState!=null){
+            //to get last state of the stopwatch
+            seconds = savedInstanceState.getInt("seconds");
+            running
+                    = savedInstanceState
+                    .getBoolean("running");
+            wasRunning= savedInstanceState
+                    .getBoolean("wasRunning");
+        }
+runTimer();
 }
+
+    public void onSaveInstanceState(
+            Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState
+                .putInt("seconds", seconds);
+        savedInstanceState
+                .putBoolean("running", running);
+        savedInstanceState
+                .putBoolean("wasRunning", wasRunning);
+    }
 }
